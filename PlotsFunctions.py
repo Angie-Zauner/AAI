a345e37f4eb31f913ce0dafed996ffb163e5e86e
Angie-Zauner <zauner.1883247@studenti.uniroma1.it>
2025-10-26 12:53:38 +0000

    Add files via upload

--- PlotsFunctions.py
+++ PlotsFunctions.py
@@ -3,22 +3,24 @@ import pandas as pd
 import numpy as np
 import math
 
-def plot_distributions(df, columns, palette="Set2"):
+def plot_distributions(df, columns, ncol=4, color="lightskyblue"):
     """
     Plot the distributions of the specified columns in the DataFrame.
 
     Parameters:
     df (pd.DataFrame): The input DataFrame.
     columns (list): List of column names to plot.
+    ncol (int): Number of subplots per column.
+    color (str): Color of the plot.
     """
     num_cols = len(columns)
-    num_rows = math.ceil(num_cols / 2)
+    num_rows = math.ceil(num_cols / ncol)
 
-    plt.figure(figsize=(12, num_rows * 4))
+    plt.figure(figsize=(ncol*4, num_rows * 4))
 
     for i, col in enumerate(columns):
-        plt.subplot(num_rows, 2, i + 1)
-        plt.hist(df[col], bins=30, color=plt.cm.get_cmap(palette)(i), alpha=0.7)
+        plt.subplot(num_rows, ncol, i + 1)
+        plt.hist(df[col], bins=30, color=color, alpha=0.7)
         plt.title(f'Distribution of {col}')
         plt.xlabel(col)
         plt.ylabel('Frequency')
